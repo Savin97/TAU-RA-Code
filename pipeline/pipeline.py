eb052b914f9d70e858f413733bc955f8fbdf60ee
Savin97 <47434031+Savin97@users.noreply.github.com>
2026-04-25 23:57:35 +0300

    25/4/26 tweaking n-grams output

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -172,8 +172,8 @@ def run_pipeline(system, n):
     print(list(n_gram_dict.keys()))
     for key,item in n_gram_dict.items():
         print(f"{key} number of n-progs: {len(item)}")
-        text.write()
-
+        text.write(f"{key}\t{item} ")
+    print("Created n-grams.txt")
     all_n_grams = n_gram_dict["All"]
     top_100 = all_n_grams.most_common(100)
     print(top_100)
